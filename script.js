4974e6575ada1db2bbf6d40dca89291d2ccae37f
Nicholas Hickman <162548631+Synetraa1@users.noreply.github.com>
2024-12-18 09:40:32 +0200

    updates
    updated
    projects
    background
    altered some text

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// Get all images with the class 'projectImg'
+var images = document.querySelectorAll(".projectImg");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("projectImg");
+// Get the modal, modal image, and caption elements
+var modal = document.getElementById("myModal");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-img.onclick = function() {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+// Loop through each image and add the click event
+images.forEach(function(image) {
+    image.onclick = function() {
+        modal.style.display = "block"; // Show the modal
+        modalImg.src = this.src; // Set the modal image to the clicked image's source
+        captionText.innerHTML = this.alt; // Set the caption to the clicked image's alt text
+    }
+});
 
-// When the user clicks on <span> (x), close the modal
+// Get the close button element and add click event to close the modal
 document.getElementsByClassName("close")[0].onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none"; // Close the modal
 }
 
-// When the user clicks anywhere outside of the modal image, close the modal
+// When the user clicks anywhere outside the modal image, close the modal
 modal.onclick = function(event) {
     if (event.target === modal) {
-      modal.style.display = "none";
+        modal.style.display = "none"; // Close the modal if clicked outside the image
     }
 }
